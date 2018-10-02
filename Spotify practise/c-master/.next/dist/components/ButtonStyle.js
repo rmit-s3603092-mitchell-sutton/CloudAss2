3883d7a1f0b6076de221b75ac08643a876a6f6e0
MitchellS <s3603092@student.rmit.edu.au>
2018-10-02 17:01:15 +1000

    Removed the stuff we don't need
    I kept all the node modules because it really wont affect us whether its on there, we may need to use more with time

--- Spotify practise/c-master/.next/dist/components/ButtonStyle.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var ButtonStyle = exports.ButtonStyle = ".btn {\n  background-color: transparent;\n  border: 1px solid #666;\n  border-radius: 50px;\n  color: #666;\n  cursor: pointer;\n  line-height: 28px;\n  padding: 0 15px;\n}";
-
-var ButtonDarkStyle = exports.ButtonDarkStyle = ".btn--dark {\n  background-color: #bbc8d5;\n  border: 1px solid #bbc8d5;\n  color: #333;\n}";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvQnV0dG9uU3R5bGUuanMiXSwibmFtZXMiOlsiQnV0dG9uU3R5bGUiLCJCdXR0b25EYXJrU3R5bGUiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQU8sSUFBTSxvQ0FBTixBQVVQOztBQUFPLElBQU0sNENBQU4iLCJmaWxlIjoiQnV0dG9uU3R5bGUuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL01pdGNoZWxsUy9MaWJyYXJ5L01vYmlsZSBEb2N1bWVudHMvY29tfmFwcGxlfkNsb3VkRG9jcy9STUlUL1NlbTIvQ2xvdWQgQ29tcHV0aW5nL0FzczIvU3BvdGlmeSBwcmFjdGlzZS9jLW1hc3RlciJ9
